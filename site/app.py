69b7349106c5c5bb2074f7548cb78f468466de74
Alex Miao <github@alexmiao.com>
2021-10-09 15:33:27 +0100

    added gitignore

--- site/app.py
+++ site/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Heldsfsd World!</p>"
+@app.route("/home")
+def index():
+    items = ["item2","your mum","python"]
+    return render_template("index.html",items=items)
+
+@app.route("/about/<username>")
+def about_page(username):
+    return "<h1>This is the about page of, {} </h1>".format(username)
