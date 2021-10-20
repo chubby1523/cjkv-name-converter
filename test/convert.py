a1b08692a28339b1836ada4d9eec2d454fd0afbf
Alex Miao <github@alexmiao.com>
2021-10-20 20:39:44 +0100

    app

---
+++ test/convert.py
@@ -0,0 +1,10 @@
+from polyglot.transliteration import Transliterator
+transliterator = Transliterator(source_lang="en", target_lang="ru")
+print(transliterator.transliterate(u"preprocessing"))
+
+
+#chinese simplified
+#japanese 
+#japanese shinjitai
+#korean hangul
+#vietnamese script
